97a76062a9ae707f2c16200bedd4a691a7ba27e2
Dayronaguilera <aguilera.dayron@gmail.com>
2021-06-03 18:58:09 +0200

    Refactoring

--- js/script.js
+++ js/script.js
@@ -1,27 +1,24 @@
 
 //3. stampa la lista ordinata alfabeticamente
 //4. scrivi anche la posizione “umana” (partendo da 1) 
-//della lista in cui il nuovo utente si trovaS
-
-
-
+//della lista in cui il nuovo utente si trova
 
 // inseriscilo in un array dove sono presenti già altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-var listSurname = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+var listSurname = ["Bianchi ", "Neri ", "Rossi ", "Verdi ", "Gialli "];
+var surname = prompt("inserisci il tuo cognome! ");
 
 // chiedi all’utente il cognome e va pushato dentro l'array!
-listSurname.push(prompt("inserisci il tuo cognome! "));
+listSurname.push(surname);
 
 // serve per ordinare i cognomi in ordine alfabetico!
 listSurname.sort();
-
-
 var output = document.getElementById('listaNomi');
+var i = 0;
 var listHtml = '';
 
-for (var i = 0; i < listSurname.length; i++){
+while ( i < listSurname.length) {
     listHtml += '<li>' + listSurname[i] + '</li>';
-    listSurname.indexOf("Aguilera");
+    i++ ;
 }
 
-output.innerHTML = listHtml + '</br>';
+output.innerHTML = listHtml  + "Ti trovi nella posizione N: " + parseInt(listSurname.indexOf(surname) + 1) ;
